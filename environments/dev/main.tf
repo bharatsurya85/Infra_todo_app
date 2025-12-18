4557d460d91c441aa8da30cc1a863b95aa7102f1
Bharat Surya <bharat.surya.85@gmail.com>
2025-12-18 17:09:19 +0530

    1st commit

---
+++ environments/dev/main.tf
@@ -0,0 +1,42 @@
+module "resource_group" {
+  source = "../../modules/azurerm_resource_group"
+  rgs    = var.rgs
+}
+
+module "network" {
+  depends_on      = [module.resource_group]
+  source   = "../../modules/azurerm_networking"
+  networks = var.networks
+}
+
+module "public_ip" {
+  depends_on      = [module.resource_group]
+  source     = "../../modules/azurerm_public_ip"
+  public_ips = var.public_ips
+}
+
+module "key_vault" {
+  depends_on      = [module.resource_group]
+  source     = "../../modules/azurerm_key_vault"
+  key_vaults = var.key_vaults
+}
+
+module "sql_server" {
+  depends_on      = [module.resource_group]
+  source          = "../../modules/azurerm_sql_server"
+  sql_server_name = "sql-dev-todoapp-bharat"
+  rg_name         = "bharat-rg-dev-todoapp-01"
+  location        = "centralindia"
+  admin_username  = "devopsadmin"
+  admin_password  = "P@ssw01rd@123"
+  tags            = {}
+}
+
+module "sql_db" {
+  depends_on  = [module.sql_server]
+  source      = "../../modules/azurerm_sql_database"
+  sql_db_name = "sqldb-dev-todoapp-bharat"
+  server_id   = module.sql_server.server_id
+  max_size_gb = "2"
+  tags        = {}
+}
---
+++ environments/dev/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.49.0"
+    }
+  }
+  # backend "azurerm" {
+  #   resource_group_name  = "rg-dhondu"
+  #   storage_account_name = "ttefstatesdhondhuwala"
+  #   container_name       = "tfstates"
+  #   key                  = "dev.terraform.tfstate"
+  # }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "98473d5b-c639-404e-9bf2-91559fe65ff8"
+}
